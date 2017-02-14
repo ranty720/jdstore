cbf28ca4cc94af81da5745c319af799334afb1ac
ranty <ranty720@gmail.com>
2017-02-14 18:39:19 +0800

    debug

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,7 +4,7 @@ class OrderMailer < ApplicationMailer
       @user        = order.user
       @product_lists = @order.product_lists
 
-      mail(to: "ranty@test.com" , subject: "[JDstore] 用户#{order.user.email}申请取消订单 #{order.token}")
+      mail(to: @use.email, subject: "[JDstore] 用户#{order.user.email}申请取消订单 #{order.token}")
   end
   def notify_ship(order)
     @order        = order
@@ -20,5 +20,12 @@ class OrderMailer < ApplicationMailer
     @product_lists = @order.product_lists
 
     mail(to: @user.email, subject: "[JDStore] 您的订单 #{order.token}已取消")
+  end
+  def apply_cancel(order)
+    @order       = order
+    @user        = order.user
+    @product_lists = @order.product_lists
+
+    mail(to: "admin@test.com" , subject: "[JDStore] 用户#{order.user.email}申请取消订单 #{order.token}")
   end
 end
